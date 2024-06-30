91308c78973ea87fc3567cbbbf6a1b25da781cc7
Techguruhacks <125970861+Techguruhack@users.noreply.github.com>
2024-06-30 13:51:01 +0530

    commit changes in next commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const myKeys = [
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';',
+    'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/'
+];
+
+const myMessageBox = document.getElementById('myMessageBox');
+const myKeyboard = document.getElementById('myKeyboard');
+
+myKeys.forEach(myKey => {
+    const myKeyButton = document.createElement('div');
+    myKeyButton.className = 'myKey';
+    myKeyButton.textContent = myKey;
+    myKeyButton.addEventListener('click', () => {
+        myMessageBox.textContent += myKey;
+    });
+    myKeyboard.appendChild(myKeyButton);
+});
